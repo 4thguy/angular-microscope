9239998f8a47a7ecd4917f588fc3f8d2316b9997
Ivan Ellul Xuereb <ivanxuereb@gmail.com>
2018-08-11 13:01:45 +0200

    added portrait / landscape modes for setup

--- src/app/modules/card.module.ts
+++ src/app/modules/card.module.ts
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon'
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatListModule,
     MatIconModule,
     MatCheckboxModule,
+    MatSlideToggleModule,
   ],
   exports: [
     MatCardModule,
@@ -23,6 +25,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatListModule,
     MatIconModule,
     MatCheckboxModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: []
